77fcc5f09c3f7726b9ccc00f73d7105a17d09d80
Tikhon <tihonkudinov@gmail.com>
2022-10-15 17:35:40 +0400

    add new features

--- Handwritten_recognition_6/Form1.Designer.cs
+++ Handwritten_recognition_6/Form1.Designer.cs
@@ -34,6 +34,8 @@ namespace Handwritten_recognition_6
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.hand_text)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -62,7 +64,7 @@ namespace Handwritten_recognition_6
             this.файлToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(977, 24);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -77,15 +79,35 @@ namespace Handwritten_recognition_6
             // открытьToolStripMenuItem
             // 
             this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
             this.открытьToolStripMenuItem.Text = "Открыть";
             this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(378, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(651, 103);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 229);
+            this.textBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(977, 551);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.hand_text);
             this.Controls.Add(this.menuStrip1);
@@ -107,6 +129,8 @@ namespace Handwritten_recognition_6
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Handwritten_recognition_6/Form1.cs
+++ Handwritten_recognition_6/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,31 +14,43 @@ using System.Windows.Forms;
 namespace Handwritten_recognition_6
 {
 	public partial class Form1 : Form
-    {
+	{
+
+		int lenght = 200;
+		int weight = 200;
 		public Form1()
-        {
-            InitializeComponent();
-			
+		{
+			InitializeComponent();
+
 		}
 
-        public void button1_Click(object sender, EventArgs e)
-        {
+		public void button1_Click(object sender, EventArgs e)
+		{
 			Bitmap bmp = new Bitmap(hand_text.Image);
 			Otsu.ApplyOtsuThreshold(ref bmp); // метод Отцу
 			hand_text.Image = bmp;
+			bmp.Save("diplom_black.png", System.Drawing.Imaging.ImageFormat.Png);
+
+			
+			hand_text.Image = Image.FromFile("D:\\VSProjects\\Handwritten_recognition_6\\Handwritten_recognition_6\\bin\\Debug\\diplom_black.png");
+			bool[][] t = Skeletonizator.Image2Bool(hand_text.Image);
+			t = Skeletonizator.ZhangSuenThinning(t);
+			Image img = Skeletonizator.Bool2Image(t);
+			Bitmap bitmap = new Bitmap(img);
+			bitmap.Save("diplom_skeleton.png", System.Drawing.Imaging.ImageFormat.Png);
 		}
 
-        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Bitmap bitmap;
+		private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Bitmap bitmap;
 			hand_text.AutoSize = true;
-			bitmap = new Bitmap(700, 500);
+			bitmap = new Bitmap(lenght, weight);
 			OpenFileDialog dialog = new OpenFileDialog();
 			dialog.Filter = "Изображения (*.JPG, *.GIF, *.PNG, *.BMP)|*.jpg;*.gif;*.png;*.bmp;";
 			if (dialog.ShowDialog() == DialogResult.OK)
 			{
 				Image image = Image.FromFile(dialog.FileName);
-				hand_text.Image = new Bitmap(Image.FromFile(dialog.FileName), 700, 500);
+				hand_text.Image = new Bitmap(Image.FromFile(dialog.FileName), lenght, weight);
 			}
 			else
 			{
@@ -44,8 +58,68 @@ namespace Handwritten_recognition_6
 				Graphics.FromImage(hand_text.Image).Clear(Color.White);
 				hand_text.Refresh();
 			}
-			
+
 			this.AutoSize = true;
-        }
-    }
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			Bitmap bmp = new Bitmap(hand_text.Image);
+			int[,] matrix = new int[bmp.Height, bmp.Width]; // матрица изображения из 0 и 1
+			for (int i = 0; i < bmp.Height; i++)
+			{
+				for (int j = 0; j < bmp.Width; j++)
+				{
+					if (bmp.GetPixel(j, i) == Color.FromArgb(255, 0, 0, 0)) matrix[i, j] = 1;
+				}
+			}
+			int m = matrix.GetLength(0);
+			int n = matrix.GetLength(1);
+			using (StreamWriter sw = new StreamWriter("Результат.txt"))
+			{
+				for (int i = 0; i < m; i++)
+				{
+					for (int j = 0; j < n; j++)
+					{
+						sw.Write(matrix[i, j]);
+					}
+					sw.Write("\r\n");
+				}
+			}
+			
+			var up_bord = (x:0, y:0);
+			var down_bord = (x:100, y:100);
+			var left_bord = (x:100, y:100);
+			var right_bord = (x:100, y:100);
+			//Tuple<int, int> up_bord, down_bord, left_bord, right_bord;
+			for (int i = 0; i < lenght; i++)
+            {
+                for (int j = 0; j < weight; j++)
+                {
+					if (matrix[j, j] == 1)
+                    {
+						up_bord.x = i;
+						up_bord.y = j;
+					}
+					break;
+				}
+            }
+			for (int i = lenght; i > 0; i--)
+			{
+				for (int j = weight; j > 0; j--)
+				{
+					if (matrix[j, j] == 1)
+					{
+						down_bord.x = i;
+						down_bord.y = j;
+					}
+					break;
+				}
+			}
+			textBox1.Text = up_bord.x.ToString();
+			textBox1.Text = up_bord.y.ToString();
+			textBox1.Text = down_bord.x.ToString();
+			textBox1.Text = down_bord.y.ToString();
+		}
+	}
 }
