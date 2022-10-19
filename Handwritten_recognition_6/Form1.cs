4cb7a89940e63d0e378361c8aa156c795741b3b6
Tikhon <tihonkudinov@gmail.com>
2022-10-19 18:58:24 +0400

    add crop words

--- Handwritten_recognition_6/Form1.cs
+++ Handwritten_recognition_6/Form1.cs
@@ -16,8 +16,8 @@ namespace Handwritten_recognition_6
 	public partial class Form1 : Form
 	{
 
-		int lenght = 200;
-		int weight = 200;
+		int lenght = 512;
+		int weight = 512;
 		public Form1()
 		{
 			InitializeComponent();
@@ -101,20 +101,20 @@ namespace Handwritten_recognition_6
 					sw.Write("\r\n");
 				}
 			}
-			
-			var up_bord = (x:-1, y:-1);
-			var down_bord = (x:0, y:0);
-			var left_bord = (x:0, y:1000000);
-			var right_bord = (x:0, y:0);
+
+			var up_bord = (x: -1, y: -1);
+			var down_bord = (x: 0, y: 0);
+			var left_bord = (x: 0, y: 1000000);
+			var right_bord = (x: 0, y: 0);
 			var left_bord_tmp = (x: 0, y: 1000000);
 			var right_bord_tmp = (x: 0, y: -1);
 			//Tuple<int, int> up_bord, down_bord, left_bord, right_bord;
 			for (int i = 0; i < lenght; i++) // верхняя и нижняя границы
-            {
-                for (int j = 0; j < weight; j++)
-                {
+			{
+				for (int j = 0; j < weight; j++)
+				{
 					if (matrix[i, j] == 1 && up_bord.x == -1)
-                    {
+					{
 						up_bord.x = i;
 						up_bord.y = j;
 					}
@@ -125,30 +125,30 @@ namespace Handwritten_recognition_6
 					}
 				}
 			}
-            for (int i = 0; i < lenght; i++) // левая и правая границы
-            {
-                for (int j = 0; j < weight; j++)
-                {
-                    if (matrix[i, j] == 1 && left_bord_tmp.y > j)
-                    {
+			for (int i = 0; i < lenght; i++) // левая и правая границы
+			{
+				for (int j = 0; j < weight; j++)
+				{
+					if (matrix[i, j] == 1 && left_bord_tmp.y > j)
+					{
 						left_bord_tmp = (i, j);
-                    }
+					}
 					if (matrix[i, j] == 1 && right_bord_tmp.y < j)
 					{
 						right_bord_tmp = (i, j);
 					}
 				}
 				if (left_bord.y > left_bord_tmp.y)
-                {
+				{
 					left_bord = left_bord_tmp;
-                }
+				}
 				if (right_bord.y < right_bord_tmp.y)
 				{
 					right_bord = right_bord_tmp;
 				}
 			}
-			
-			
+
+
 			using (StreamWriter sw = new StreamWriter("Результат_прямоугольник.txt"))
 			{
 				bool flag = false;
@@ -182,6 +182,137 @@ namespace Handwritten_recognition_6
 			" " + left_bord.y.ToString() +
 			" " + right_bord.x.ToString() +
 			" " + right_bord.y.ToString();
+
+			Image img = Image.FromFile("D:\\VSProjects\\Handwritten_recognition_6\\Handwritten_recognition_6\\bin\\Debug\\diplom_black.png");
+			using (Graphics gr = Graphics.FromImage(img))
+			{
+				gr.DrawRectangle(new Pen(Color.Red, 2), left_bord.y, up_bord.x, right_bord.y - left_bord.y, down_bord.x - up_bord.x);
+			}
+			hand_text.Image = img;
+
+			Bitmap crop_img = new Bitmap("diplom_crop.png");
+			int[,] crop_matrix = new int[crop_img.Height, crop_img.Width]; // матрица изображения из 0 и 1
+			for (int i = 0; i < crop_img.Height; i++)
+			{
+				for (int j = 0; j < crop_img.Width; j++)
+				{
+					if (crop_img.GetPixel(j, i) == Color.FromArgb(255, 0, 0, 0)) crop_matrix[i, j] = 1;
+				}
+			}
+
+			int crop_m = crop_matrix.GetLength(0);
+			int crop_n = crop_matrix.GetLength(1);
+			List<int> sum_of_1 = new List<int>();
+			using (StreamWriter sw = new StreamWriter("Результат_единиц_в_строках.txt"))
+			{
+				for (int i = 0; i < crop_m; i++)
+				{
+					int sum_1 = 0;
+					for (int j = 0; j < crop_n; j++)
+					{
+						if (crop_matrix[i, j] == 1)
+						{
+							sum_1++;
+						}
+					}
+					sum_of_1.Add(sum_1);
+					sw.Write(sum_1);
+					sw.Write("\r\n");
+				}
+			}
+
+			int max_in_list = sum_of_1.Max();
+			int idx = 0;
+			for (int i = 0; i < sum_of_1.Count(); i++)
+			{
+				if (sum_of_1[i] >= max_in_list)
+				{
+					idx = i;
+				}
+			}
+			textBox1.Clear();
+			textBox1.Text = idx.ToString();
+
+			int sum_0 = 0, j_left = 0;
+			List<(int, int, int)> sum_of_0 = new List<(int, int, int)>();
+
+			int numb_btw_words = 45;
+
+			for (int j = 0; j < crop_n; j++)
+			{
+				if (j == crop_n - 1)
+					break;
+				if (crop_matrix[idx, j] == 1 && crop_matrix[idx, j + 1] == 0)
+				{
+					sum_0 = 0;
+					j_left = j;
+					do
+					{
+						sum_0++;
+						j++;
+						if (j == crop_n - 1)
+							break;
+					}
+					while (crop_matrix[idx, j + 1] == 0);
+					sum_of_0.Add((j_left, j + 1, sum_0));
+				}
+			}
+			using (StreamWriter sw = new StreamWriter("Результат_нулей_в_строке.txt"))
+			{
+				for (int i = 0; i < sum_of_0.Count; i++)
+				{
+					sw.Write(sum_of_0[i]);
+					sw.Write("\r\n");
+				}
+			}
+			List<(int, int, int)> find_words = new List<(int, int, int)>();
+			for (int i = 0; i < sum_of_0.Count; i++)
+			{
+				if (sum_of_0[i].Item3 >= numb_btw_words)
+				{
+					find_words.Add(sum_of_0[i]);
+				}
+			}
+			for (int i = 0; i < find_words.Count; i++)
+			{
+				Console.WriteLine(find_words[i]);
+			}
+
+			List<int> words_borders = new List<int>();
+			words_borders.Add(0);
+
+			for (int k = 0; k < find_words.Count; k++)
+			{
+				for (int j = find_words[k].Item1; j < find_words[k].Item2; j++)
+				{
+					int i;
+					for (i = 0; i < crop_m; i++)
+					{
+						if (crop_matrix[i, j] == 1)
+						{
+							break;
+						}
+					}
+					if (i == crop_m)
+					{
+						words_borders.Add(j);
+						break;
+					}
+				}
+			}
+			words_borders.Add(crop_n);
+			foreach (var item in words_borders)
+			{
+				Console.WriteLine(item);
+			}
+			Image img_crop = Image.FromFile("diplom_crop.png");
+			for (int i = 0; i < words_borders.Count - 1; i++)
+			{
+				Image img_crop_1 = (Image)img_crop.Clone();
+				Image cropimage_words = Crop(img_crop_1, new Rectangle(words_borders[i], 0, words_borders[i + 1] - words_borders[i], crop_m));
+				Bitmap bitmap1 = new Bitmap(cropimage_words);
+				bitmap1.Save("D:\\VSProjects\\Handwritten_recognition_6\\Handwritten_recognition_6\\bin\\Debug\\Words\\word_" + i + ".png", System.Drawing.Imaging.ImageFormat.Png);
+			}
 		}
 	}
 }
